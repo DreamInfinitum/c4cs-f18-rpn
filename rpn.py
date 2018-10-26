6ffc11f6934acbde91874a7d527b79ab8da9297c
Siyu Wu <wusiyu@umich.edu>
2018-10-26 15:02:10 -0400

    Implment ADD

---
+++ rpn.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+def calculate(arg):
+	stack = []
+
+	tokens = arg.split()
+
+	for token in tokens:
+		try:
+			stack.append(int(token))
+		except ValueError:
+			val1 = stack.pop()
+			val2 = stack.pop()
+			result = val1 + val2
+
+			stack.append(result)
+
+	return stack[0]
+
+def main():
+	while True:
+		result = calculate(input('rpn calc> '))
+		print(result)
+
+if _name_ == '_main_':
+	main()
