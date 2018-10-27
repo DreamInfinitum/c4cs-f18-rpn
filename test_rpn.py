e3f9f87620e8d69f7d7960c2d98f671802c37887
Siyu Wu <wusiyu@umich.edu>
2018-10-27 02:11:50 -0400

    Forth Commit

--- test_rpn.py
+++ test_rpn.py
@@ -11,10 +11,10 @@ class TestBasics(unittest.TestCase):
 		result = rpn.calculate('4 6 -')
 		self.assertEqual(-2, result)
 
-	def test_toomany_add(self)
+	def test_toomany_add(self):
 		with self.assertRaises(ValueError):
 			result = rpn.calculate('1 2 3 +')
 
-	def test_toomany_sub(self)
+	def test_toomany_sub(self):
 		with self.assertRaises(ValueError):
 			result = rpn.calculate('1 2 3 -')
